cbdd964276573cec5c489cf259de1fa67fd4b262
theurtebize <t.heurtebize@outlook.com>
2020-02-13 16:45:08 +0100

    Update bot.ps1

--- bot.ps1
+++ bot.ps1
@@ -1,4 +1,6 @@
 Import-Module "c:\lib\script1.ps1"
+#netstat -ano | find "1984"
+$Socket = FRX_Socket-Listen-Connect -port 1984
 
 $version = 1
 
@@ -7,8 +9,7 @@ $gitLink = 'https://raw.githubusercontent.com/theurtebize/majBOT/master/bot.ps1?
 # Récupération du git
 Invoke-WebRequest -Uri $gitLink -UseBasicParsing -OutFile 'C:\lib\botv1.ps1'
 
-#netstat -ano | find "1984"
-$Socket = FRX_Socket-Listen-Connect -port 1984
+#test
 
 
 $closeACK = FRX_Socket-Listen-Close -socket $Socket

